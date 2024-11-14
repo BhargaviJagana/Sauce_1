337c2f6cd8534ac6f662cc80296be37a53dba2fd
BhargaviJagana <106737216+BhargaviJagana@users.noreply.github.com>
2024-11-14 14:11:28 +0530

    Create PageObjects/Login/LoginPage_Action.cs

---
+++ PageObjects/Login/LoginPage_Action.cs
@@ -0,0 +1,7 @@
+// LoginPage_Action.cs
+
+namespace PageObjects.Login {
+    public partial class LoginPage {
+        // Define actions for interacting with the login page using locators from LoginPage_Locator.cs
+    }
+}
