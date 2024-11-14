68c7ab2ffbba484fb6a7a0c465b9201848ae6aa0
BhargaviJagana <106737216+BhargaviJagana@users.noreply.github.com>
2024-11-14 14:11:14 +0530

    Create Utilities/WebUtilities/WebUtilities.cs

---
+++ Utilities/WebUtilities/WebUtilities.cs
@@ -0,0 +1,7 @@
+// WebUtilities.cs
+
+namespace Utilities.WebUtilities {
+    public class WebUtilities {
+        // Implement methods to interact with web elements using IWebDriver wrapped inside WebUtilities
+    }
+}
