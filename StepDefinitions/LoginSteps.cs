7b22fa2baaf75f5932b06d6b69893f87e8a93d14
BhargaviJagana <106737216+BhargaviJagana@users.noreply.github.com>
2024-11-14 14:11:40 +0530

    Create StepDefinitions/LoginSteps.cs

---
+++ StepDefinitions/LoginSteps.cs
@@ -0,0 +1,10 @@
+// LoginSteps.cs
+
+using TechTalk.SpecFlow;
+
+namespace StepDefinitions {
+    [Binding]
+    public class LoginSteps {
+        // Implement step definitions for Login.feature
+    }
+}
